bcf1c98b873801d0b931b87932079a7fdf83249a
Asiimoviet <asiimoviet@gmail.com>
2021-09-13 14:04:10 +0800

    added CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/asiimoviet/Repositories/ScheduleReader2.0
+# Build directory: /Users/asiimoviet/Repositories/ScheduleReader2.0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
